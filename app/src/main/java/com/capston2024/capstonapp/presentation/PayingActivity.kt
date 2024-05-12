db9e454e0799951925f3252f9b65131cb7bd25ac
sjtw09 <tanyatw11555@kyonggi.ac.kr>
2024-05-13 00:16:50 +0900

    [시작화면, 끝화면 new figma 디자인 적용, 앱 실행시 StartActivity 나오도록 수정]

--- app/src/main/java/com/capston2024/capstonapp/presentation/PayingActivity.kt
+++ app/src/main/java/com/capston2024/capstonapp/presentation/PayingActivity.kt
@@ -1,19 +1,19 @@
-package com.cookandroid.myapplication
+package com.capston2024.capstonapp.presentation
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import androidx.appcompat.app.AppCompatActivity
+import com.capston2024.capstonapp.R
 
-class PayingActivity : AppCompatActivity() {
+class PayingActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_paying)
-
         Handler(Looper.getMainLooper()).postDelayed({
-            var intent = Intent(applicationContext, CompletePayment::class.java)
+            var intent = Intent(applicationContext, CompletePaymentActivity::class.java)
             startActivity(intent)
         }, 5000)
     }
