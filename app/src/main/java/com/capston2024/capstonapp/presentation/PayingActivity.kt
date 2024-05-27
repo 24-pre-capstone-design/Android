58235b5f15b0c9130c59a0e0011d8a7c191c9d07
sjtw09 <tanyatw11555@kyonggi.ac.kr>
2024-05-27 14:48:26 +0900

    [payingBtn 누르면 결제창 팝업 뜨는 것으로 변경, payingActivity 내용 mainActivity로 옮기기고 ]

--- app/src/main/java/com/capston2024/capstonapp/presentation/PayingActivity.kt
+++ app/src/main/java/com/capston2024/capstonapp/presentation/PayingActivity.kt
@@ -1,10 +1,14 @@
 package com.capston2024.capstonapp.presentation
 
+import android.animation.ObjectAnimator
 import android.app.Activity
+import android.app.Dialog
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.widget.Button
+import android.widget.ProgressBar
 import com.capston2024.capstonapp.R
 
 class PayingActivity : Activity() {
@@ -12,9 +16,37 @@ class PayingActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_paying)
+
+        val payingBtn: Button = findViewById(R.id.payingBtn)
+        payingBtn.setOnClickListener {
+            showPopup()
+        }
+    }
+
+    private fun showPopup() {
+        val payingDL = Dialog(this)
+        payingDL.setContentView(R.layout.popup_paying)
+        payingDL.setCancelable(false)
+        payingDL.show()
+
+        val payingPB: ProgressBar = payingDL.findViewById(R.id.payingProgressBar)
+        animateProgressBar(payingPB)
+
         Handler(Looper.getMainLooper()).postDelayed({
-            var intent = Intent(applicationContext, CompletePaymentActivity::class.java)
-            startActivity(intent)
+            payingDL.dismiss()
+            goToNextScreen()
         }, 5000)
+    }
+
+    private fun animateProgressBar(progressBar: ProgressBar) {
+        val animator = ObjectAnimator.ofInt(progressBar, "progress", 0, 100)
+        animator.duration = 4700
+        animator.start()
+    }
+
+    private fun goToNextScreen() {
+        val intent = Intent(this, CompletePaymentActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
